cadbfc3bff52eabf52f914ad4e33519500fb3a36
月光下的恋人 <15323771186@163.com>
2019-03-11 11:33:50 +0800

    add chooseLine

--- src/components/lineSize.vue
+++ src/components/lineSize.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="lineSize">
     <ol class="sizes">
-      <li id="thin" :class="item" v-for="(item, index) in size" :key="index"></li>
+      <li
+        id="thin"
+        :class="item"
+        @click="chooseLine(index)"
+        v-for="(item, index) in size"
+        :key="index"
+      >
+        <span :class="choose" v-show="index===lineIndex"></span>
+      </li>
     </ol>
   </div>
 </template>
@@ -11,8 +19,14 @@ export default {
   name: "LineSize",
   data() {
     return {
-      size: ["thin", "thick"]
+      size: ["thin", "thick"],
+      lineIndex: 0
     };
+  },
+  methods: {
+    chooseLine(index) {
+      this.lineIndex = index;
+    }
   }
 };
 </script>
@@ -29,16 +43,30 @@ ol {
   top: 250px;
   li {
     margin: 20px 0;
+    position: relative;
+    span {
+      display: block;
+      width: 0;
+      height: 0;
+      border-top: 5px solid transparent;
+      border-left: 5px solid transparent;
+      border-right: 5px solid transparent;
+      border-bottom: 5px solid red;
+      position: absolute;
+      top: 100%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
   }
   & > .thin {
     height: 0;
     width: 30px;
-    border-top: 3px solid black;
+    border-top: 5px solid black;
   }
   & > .thick {
     height: 0;
     width: 30px;
-    border-top: 6px solid black;
+    border-top: 10px solid black;
   }
 }
 </style>
